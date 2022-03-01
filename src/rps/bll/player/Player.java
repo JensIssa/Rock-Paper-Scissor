4f92f5f4b7fcdfedbd3d1aee77608f794e313f09
Jens Bassem Hanna Issa <jens7391@easv365.dk>
2022-03-01 14:27:44 +0100

    countScoresMethod thingy

--- src/rps/bll/player/Player.java
+++ src/rps/bll/player/Player.java
@@ -6,6 +6,7 @@ import rps.bll.game.Move;
 import rps.bll.game.Result;
 
 //Java imports
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -19,6 +20,9 @@ public class Player implements IPlayer {
 
     private String name;
     private PlayerType type;
+    private int paperPick;
+    private int rockPick;
+    private int scissorsPick;
 
     /**
      * @param name
@@ -41,6 +45,23 @@ public class Player implements IPlayer {
     }
 
 
+
+
+    private ArrayList<Result> countScores(ArrayList<Result> countResults){
+        if(!countResults.isEmpty()) {
+            Result lastMove = countResults.get(countResults.size() - 1);
+            if (lastMove.getWinnerMove() == Move.Paper){
+                paperPick++;
+            }
+            if (lastMove.getWinnerMove() == Move.Rock){
+                rockPick++;
+            }
+            if(lastMove.getWinnerMove() == Move.Scissor){
+                scissorsPick++;
+            }
+        }
+        return countResults;
+    }
     /**
      * Decides the next move for the bot...
      * @param state Contains the current game state including historic moves/results
@@ -50,13 +71,16 @@ public class Player implements IPlayer {
     public Move doMove(IGameState state) {
         //Historic data to analyze and decide next move...
         ArrayList<Result> results = (ArrayList<Result>) state.getHistoricResults();
+        Move move = null;
+        System.out.println(paperPick);
 
-
-
-        Move result = randomMove();
-
-        return result;
+        if(results.size() < 1 ){
+            move = Move.Paper;
+        }
+        countScores(results);
+        return move;
     }
+
     private Move randomMove(){
         Random randomMove = new Random();
         int i = randomMove.nextInt(4);
